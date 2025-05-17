508dbb36830ccec1d192bbf266b88d48af58354d
sefler <sefler@126.com>
2025-05-17 12:13:24 +0800

    exp high performance

--- libfreerdp/codec/h264_openh264.c
+++ libfreerdp/codec/h264_openh264.c
@@ -208,9 +208,9 @@ static int openh264_compress(H264_CONTEXT* h264, const BYTE** pYUVData, const UI
 		sys->EncParamExt.iMaxBitrate = UNSPECIFIED_BIT_RATE;
 		sys->EncParamExt.bEnableDenoise = 0;
 		sys->EncParamExt.bEnableLongTermReference = 0;
-		sys->EncParamExt.bEnableFrameSkip = 0;
+		sys->EncParamExt.bEnableFrameSkip = 1;
 		sys->EncParamExt.iSpatialLayerNum = 1;
-		sys->EncParamExt.iMultipleThreadIdc = (int)h264->NumberOfThreads;
+		sys->EncParamExt.iMultipleThreadIdc = 8;
 		sys->EncParamExt.sSpatialLayers[0].fFrameRate = h264->FrameRate;
 		sys->EncParamExt.sSpatialLayers[0].iVideoWidth = sys->EncParamExt.iPicWidth;
 		sys->EncParamExt.sSpatialLayers[0].iVideoHeight = sys->EncParamExt.iPicHeight;
