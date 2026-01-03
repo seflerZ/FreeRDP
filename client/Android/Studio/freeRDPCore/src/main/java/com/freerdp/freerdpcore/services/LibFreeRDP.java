2b23928c40a1484747bb63555bf83f51b4f94f16
sefler <sefler@126.com>
2026-01-03 21:07:43 +0800

    try to resolve the unexpected crash during sending keydown or cursor movement events

--- client/Android/Studio/freeRDPCore/src/main/java/com/freerdp/freerdpcore/services/LibFreeRDP.java
+++ client/Android/Studio/freeRDPCore/src/main/java/com/freerdp/freerdpcore/services/LibFreeRDP.java
@@ -311,8 +311,8 @@ public class LibFreeRDP
 
 //		args.add(addFlag("auto-reconnect", true));
 		args.add(addFlag("fast-path", true));
-		args.add(addFlag("async-input", false));
-		args.add(addFlag("async-update", true));
+		args.add(addFlag("async-input", true));
+		args.add(addFlag("async-update", false));
 		args.add(addFlag("bitmap-cache", true));
 		args.add(addFlag("async-channels", false));
 		args.add(addFlag("compression", false));
