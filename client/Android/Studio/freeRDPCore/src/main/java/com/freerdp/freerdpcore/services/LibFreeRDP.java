6d9eb11b1588d248d80e4c40f96e3716cc192c5d
sefler <sefler@126.com>
2026-02-25 23:27:35 +0800

    disable async channel for it may crash program when sending inputs

--- client/Android/Studio/freeRDPCore/src/main/java/com/freerdp/freerdpcore/services/LibFreeRDP.java
+++ client/Android/Studio/freeRDPCore/src/main/java/com/freerdp/freerdpcore/services/LibFreeRDP.java
@@ -322,7 +322,7 @@ public class LibFreeRDP
 		args.add(addFlag("async-input", true));
 		args.add(addFlag("async-update", true));
 		args.add(addFlag("bitmap-cache", true));
-		args.add(addFlag("async-channels", true));
+		args.add(addFlag("async-channels", false));
 		args.add(addFlag("compression", false));
 
 		args.add(addFlag("dynamic-resolution", true));
