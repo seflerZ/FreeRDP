7b57452021a3c4e75efde7029b11158c7a159701
sefler <sefler@126.com>
2026-05-31 21:18:19 +0800

    improve compression

--- libfreerdp/core/settings.c
+++ libfreerdp/core/settings.c
@@ -359,7 +359,7 @@ rdpSettings* freerdp_settings_new(DWORD flags)
 	settings->CompressionEnabled = TRUE;
 	settings->LogonNotify = TRUE;
 	settings->BrushSupportLevel = BRUSH_COLOR_FULL;
-	settings->CompressionLevel = PACKET_COMPR_TYPE_RDP61;
+	settings->CompressionLevel = PACKET_COMPR_TYPE_RDP8;
 	settings->Authentication = TRUE;
 	settings->AuthenticationOnly = FALSE;
 	settings->CredentialsFromStdin = FALSE;
